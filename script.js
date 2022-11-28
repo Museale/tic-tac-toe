f6c08a44f6c9ec9361e687473cb47c9c45313175
museale <talebjorn@gmail.com>
2022-11-28 15:18:58 +0100

    check tie

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const Gameboard = (() => {
-
       let index = 0;
-      
       let board = {
         cells: [],
       }
+
       const _placeCells = (() => {
           Array.from(document.querySelectorAll("#gameboard-container > div"))
             .forEach(element => {
@@ -39,27 +38,33 @@ const PlayerFactory = (name, marker) => {
 
 const gamePlay = (() => {
       let turn = false; 
-      Gameboard.board.cells.forEach(element => {  
+      let winner = '';
+      let tie = 0;
+      const cells =  Gameboard.board.cells;
+    
+      cells.forEach(element => {  
         element.addEventListener('click', (e) => {
-       if (!turn && !e.target.classList.contains('O')) {
-          e.target = {
-            marker: 'X',
+        tie++;
+        if (!winner) {
+            if (tie == 9) {
+              console.log('tie')
+            }
+            if (!turn && !e.target.classList.contains('O')) {
+              e.target.classList.add('X');
+               turn = true;
+               }
+             else if (turn && !e.target.classList.contains('X')) {
+               e.target.classList.add('O');
+               turn = false;
+               }
           }
-          console.log(e.target);           
-           e.target.classList.add('X');
-            turn = true;
-            }
-          else if (turn && !e.target.classList.contains('X')) {
-            e.target.classList.add('O');
-            turn = false;
-            }
-          });
+          
+      });
        
 });
 
   let checkWin = () => {
-    const cells =  Gameboard.board.cells;
-    
+
        let rowOne = [cells[0], cells[1], cells[2]];
        let rowTwo = [cells[3], cells[4], cells[5]];
        let rowThree = [cells[6], cells[7], cells[8]];
@@ -70,40 +75,36 @@ const gamePlay = (() => {
        let diagonalTwo = [cells[2], cells [4], cells[6]];
 
        let win =  [rowOne, rowTwo, rowThree, columnOne, columnTwo, columnThree, diagonalOne, diagonalTwo];
-       console.log(win)
-   
        
         win.forEach(element => {
-      
-
           element.map((i) => { 
-         
             if (i.classList.contains('X') && !element.includes('O')) {
             element.push('X')
               if (element.length > 5) {
-                console.log('WUBBAWUBDUB X wins');
-                return;
+                winner = 'X';
+                console.log('WUBBAWUBDUB X wins', winner);
+                return winner;
               }
             }
           
           if (i.classList.contains('O') && !element.includes('X')) {
             element.push('O')
             if (element.length > 5) {
-              console.log('WUBBAWUBDUB O wins');
-              return;
+              winner = 'O';
+              console.log('WUBBAWUBDUB O wins', winner);
+              return winner;
+              }
             }
-            }
-          
           })
-          
         })
-
-       
 };
-document.body.addEventListener('click', () => {checkWin()});
+
+document.body.addEventListener('click', () => {!winner ? checkWin() : false});
+
  return {
   turn,
   checkWin
  }
+
 })();
 
