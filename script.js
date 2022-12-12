f01bdbbc0019c911d4971452cd1b409d51955de0
museale <talebjorn@gmail.com>
2022-12-12 11:19:40 +0100

    refactor script

--- script.js
+++ script.js
@@ -144,108 +144,132 @@
 // })()
 
 
-(function () {
+
   let index = 0;
   let tie = 0;
   let winner = '';
   let turn = false;
-  const gameBoard = {
+  let cells = [];
+  
 
-    cells: [],
-    
-    init: function () {
-      this.cacheDom();
-      this.placeCells();
-      this.bindEvents();
-    },
-
-    cacheDom: function () {
+  const gameBoard = (() => {
+    let gameboardObj = {
+      
+    // init: (function () {
+    //   this.placeCells();
+    //   this.cacheDom;
+    //   this.bindEvents; 
+    //   console.log(placeCells, cacheDom, bindEvents)
+    // })(),
+  
+    cacheDom: (function () {
       this.modal= document.getElementById('modal');
       this.modalPara = document.getElementById('modal-para');
       this.computerBtn = document.getElementById('computer-btn');
       this.clearBtn = document.getElementById('clear');
       this.startBtn =  document.getElementById('start');
-      this.gameboardContainerDivs = document.querySelectorAll('#gameboard-container > div');
+      this.gameoardContainer = document.getElementById('gameboard-container');
+      this.gameboardContainerDivs = Array.from(document.querySelectorAll('#gameboard-container > div'));
       this.playerOne = () => document.getElementById('player-one').value;
       this.playerTwo =   () => document.getElementById('player-two').value;
-    },
+    })(),
 
-    placeCells: function (){
-      Array.from(this.gameboardContainerDivs).forEach(element => {
+    placeCells: (function(){
+      this.gameboardContainerDivs.forEach(element => {
           element.value = index++;
           element.classList.add('cell');
-          this.cells.push(element);
-        console.log(gameBoard.cells)
-      });
-    },
-   
-    bindEvents: function () {
+          cells.push(element);
+          console.log(cells)
+      })
+    })(),     
+    
+    bindEvents: (function () {
       this.clearBtn.addEventListener('click', () => {
         window.location.reload();
       });
       this.startBtn.addEventListener('click', () => {
         this.modal.style = 'opacity: 1';
-        this.modalPara.textContent =  `May the best one win ${playerOne} is X and ${playerTwo} is O`;
+        this.modalPara.textContent =  `May the best one win ${this.playerOne()} is X and ${this.playerTwo()} is O`;
       });
-      this.cells.forEach(element => {
-        element.addEventListener('click', (e) => {
-          this.checkWin(e);
-        })
-      });
-      document.body.addEventListener('click', () => !winner ? this.checkWin() : false);
-   
-    }, 
+      cells.forEach(element => {
+        console.log(cells)
+          this.gameoardContainer.addEventListener('click', (e) => !winner ? console.log(this.checkplay.checkWin(e.target)) : false);
+          element.addEventListener('click', (e) => {
+            console.log('woo')
+            tie++;
+          this.checkplay.checkWin(e.target)
+        });
+     })
 
-
-
-    checkWin: function (e) {
-      this.cells.forEach(element =>  {
-        tie++;
-        if (!winner) {
-          if (tie == 9) {
-            console.log('tie')
-            this.modalPara.textContent = "EVERYBODY'S A LOSER."
-            this.modal.style = "opacity: 1"
-          }
-          if (!turn && !e.target.classList.contains('O')) {
-            e.target.classList.add('X');
-            turn = true;
-            }
-          else if (turn && !e.target.classList.contains('X')) {
-            e.target.classList.add('O');
-            turn = false;
-            }}
-      });
-
-      Array.from(this.win).forEach(element => {
-        Array.from(element).map((i) => {
-          if (i.classList.contains('X') && !element.includes('O')) {
-            element.push('X')
-              if (element.length > 5) {
-                this.winner = 'X';
-                console.log('WUBBAWUBDUB X wins', winner);
-                this.modal.style = "opacity: 1";
-                this.modalPara.textContent = `Congratulations ${playerOne()} X WINS!`;
-                return this.winner;
-                }
-              }
-            if (i.classList.contains('O') && !element.includes('X')) {
-            element.push('O')
-              if (element.length > 5) {
-                winner = 'O';
-                console.log('WUBBAWUBDUB O wins', winner);
-                this.modalPara.textContent = `Congratulations ${playerTwo()} O WINS!`;
-                this.modal.style = "opacity: 1";
-                return winner;
-                }
-              }
-        })
-      })
-},
-
-  } 
-   
+  })()
+  
+  }
+  return {gameboardObj}
+  })();
   
 
-gameBoard.init();
+const gameplay =  (() => {
+  const checkplay =  {
+
+  bindEvents: (function () {
+  
+})(),
+
+
+  win: {
+    rowOne: [cells[0], cells[1], cells[2]],
+    reowTwo: [cells[3], cells[4], cells[5]],
+    rowThree: [cells[6], cells[7], cells[8]],
+    columnOne: [cells[0], cells[3], cells[6]],
+    columnTwo: [cells[1], cells[4], cells[7]],
+    columnThree:  [cells[2], cells[5], cells[8]],
+    diagonalOne: [cells[0], cells[4], cells[8]],
+    diagonalTwo:[cells[2], cells[4], cells[6]],
+  },
+
+  checkWin: function (e) {
+    if (!winner) {
+      if (tie == 9) {
+        console.log('tie')
+        this.modalPara.textContent = "EVERYBODY'S A LOSER."
+        this.modal.style = "opacity: 1"
+      }
+        if (!turn && !e.target.classList.contains('O')) {
+          e.target.classList.add('X');
+          turn = true;
+          }
+        else if (turn && !e.target.classList.contains('X')) {
+          e.target.classList.add('O');
+          turn = false;
+          }
+    }
+
+  for(element in this.win) {
+    console.log(element)
+      Array.from(element).map((i) => {
+        if (i.classList.contains('X') && !element.includes('O')) {
+          element.push('X')
+            if (element.length > 5) {
+              this.winner = 'X';
+              console.log('WUBBAWUBDUB X wins', winner);
+              this.modal.style = "opacity: 1";
+              this.modalPara.textContent = `Congratulations ${playerOne()} X WINS!`;
+              return this.winner;
+              }
+            }
+          if (i.classList.contains('O') && !element.includes('X')) {
+          element.push('O')
+            if (element.length > 5) {
+              winner = 'O';
+              console.log('WUBBAWUBDUB O wins', winner);
+              this.modalPara.textContent = `Congratulations ${playerTwo()} O WINS!`;
+              this.modal.style = "opacity: 1";
+              return winner;
+              }
+            }
+        })
+      }
+    }
+  };
+  return {checkplay};
 })();
